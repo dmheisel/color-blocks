d82851e337cf3b58492fd748c463654cd7fde2a2
dmheisel <d.m.heisel@gmail.com>
2019-07-25 14:13:01 -0500

    update styles, add delete handler

--- script.js
+++ script.js
@@ -2,35 +2,44 @@
 
 console.log('js sourced');
 
-$(document).ready(readyNow);
+$(document).ready(handleReady);
 
-function readyNow() {
-  $('#redButton').on('click', buttonHandler);
-  $('#yellowButton').on('click', buttonHandler);
-  $('#blueButton').on('click', buttonHandler);
-  $('#greenButton').on('click', buttonHandler);
+function handleReady() {
+	$('#redButton').on('click', handleClick);
+	$('#yellowButton').on('click', handleClick);
+	$('#blueButton').on('click', handleClick);
+	$('#greenButton').on('click', handleClick);
+	$('#blockContainer').on('click', '.block', handleDelete);
 }
 
-const buttonHandler = () => {
-  //buttonHandler fetches color of button, then uses that to run addBlock function with that color
-  let color = event.target.id.replace(/Button/, '');
-  addBlock(color);
-  countBlocks(color);
+const handleClick = () => {
+	//handleClick fetches color of button, then uses that to run addBlock function with that color
+	let color = event.target.id.replace(/Button/, '');
+	addBlock(color);
+	countBlocks(color);
 };
 
+function handleDelete() {
+	let color = event.target.id.replace(/ block/, '');
+	$(this).remove();
+	countBlocks(color);
+}
+
 const addBlock = color => {
-  /* addBlock adds a block of color passed in to the DOM blockContainer element. */
-  console.log(`adding block of color ${color}`);
-  $('#blockContainer').append(`<li class="${color} block"></li>`);
+	/* addBlock adds a block of color passed in to the DOM blockContainer element. */
+	console.log(`adding block of color ${color}`);
+	$('#blockContainer').append(`<li class="${color} block"></li>`);
 };
 
 const countBlocks = color => {
-  let counter = 0;
-  let colorBlocks = $('#blockContainer li');
-  colorBlocks.each(function() {
-    if ($(this).hasClass(`${color}`)) {
-      counter++;
-    }
-  });
-  console.log(counter);
+	let counter = 0;
+	// increase counter for every li with class containing the color
+	$('.block').each(function() {
+		if ($(this).hasClass(`${color}`)) {
+			counter++;
+		}
+	});
+	let buttonId = $(`#${color}Button`);
+
+	buttonId.text(counter).addClass(`${color} clicked`);
 };
