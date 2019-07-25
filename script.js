bbe2a2e3b19f3ef3df87a6c2be32ec90a0da53ec
dmheisel <d.m.heisel@gmail.com>
2019-07-25 09:06:55 -0500

    Addtl jquery to begin adding block counter func

--- script.js
+++ script.js
@@ -15,10 +15,22 @@ const buttonHandler = () => {
   //buttonHandler fetches color of button, then uses that to run addBlock function with that color
   let color = event.target.id.replace(/Button/, '');
   addBlock(color);
+  countBlocks(color);
 };
 
 const addBlock = color => {
   /* addBlock adds a block of color passed in to the DOM blockContainer element. */
   console.log(`adding block of color ${color}`);
   $('#blockContainer').append(`<li class="${color} block"></li>`);
+};
+
+const countBlocks = color => {
+  let counter = 0;
+  let colorBlocks = $('#blockContainer li');
+  colorBlocks.each(function() {
+    if ($(this).hasClass(`${color}`)) {
+      counter++;
+    }
+  });
+  console.log(counter);
 };
