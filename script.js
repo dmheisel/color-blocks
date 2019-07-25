04cc857a99579bf3901ce0ef996af3c985ccf780
dmheisel <d.m.heisel@gmail.com>
2019-07-24 21:23:45 -0500

    Begin jquery work found method to return the string of event.target.id and remove text

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 //js script
 
-console.log('js sourced')
+console.log('js sourced');
+
+$(document).ready(readyNow);
+
+function readyNow() {
+  $('#redButton').on('click', buttonHandler);
+  $('#yellowButton').on('click', buttonHandler);
+  $('#blueButton').on('click', buttonHandler);
+  $('#greenButton').on('click', buttonHandler);
+}
+
+const buttonHandler = () => {
+  let color = event.target.id.replace(/Button/, '');
+};
+
+const addBlock = color => {
+  console.log(`adding block of color ${color}`);
+  let targetElem = $('blockContainer');
+  targetElem.append(`<li class="${color} block"></li>`);
+};
